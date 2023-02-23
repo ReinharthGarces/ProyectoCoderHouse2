52a5494009a5b2870b0057274d82d70d6fdfef38
Reinharth Garces <rjgarces7@gmail.com>
2023-02-22 21:36:08 -0300

    Add de DOM y localStore

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-// PreEntrega2+Garces
+// PreEntrega3+Garces
 
-// clase
+// clases
+
 class Cliente {
   constructor(name,lastName,Ndni,metodoPago,cantCuotas,intereses){
-      this.name = name;
-      this.lastName = lastName;
-      this.Ndni = Ndni;
-      this.metodoPago=metodoPago;
-      this.cantCuotas=cantCuotas;
-      this.intereses=intereses;
+    this.name = name;
+    this.lastName = lastName;
+    this.Ndni = Ndni;
+    this.metodoPago=metodoPago;
+    this.cantCuotas=cantCuotas;
+    this.intereses=intereses;
   }
 }
 
@@ -38,62 +39,61 @@ alert("Cliente: " + informacionCliente)
 //Metodos de pago
 
 const metodoDePago = parseInt(prompt("Indiquenos el metodo de pago que desea utilizar: \n\n 1- Tarjeta de debito \n 2- Tarjeta de credito"))
-  // var cantidadDeCuotas = 0
 
 switch (metodoDePago) {
   case 1:
-    console.log("El metodo de pago elegido es tarjeta de debito")
-    break
+  console.log("El metodo de pago elegido es tarjeta de debito")
+  break
 
   case 2:
-    console.log("El metodo de pago elegido es tarjeta de credito")
-    break
+  console.log("El metodo de pago elegido es tarjeta de credito")
+  break
 
   default:
-    alert("El metodo seleccionado es INCORRECTO")
-    break
+  alert("El metodo seleccionado es INCORRECTO")
+  break
 }
 
 
 if (metodoDePago === 1) {
   alert("Usted selecciono Tarjeta de Debito como metodo de pago, presione ACEPTAR para continuar.")
   console.log ("Finalizar programa.")
-    } else if (metodoDePago === 2){
-      var cantidadDeCuotas = parseInt(prompt(" Indiquenos la cantidad de cuotas con las que desea abonar, ( siendo valores validos 3, 6, 9 y 12 ) para confirmarle el monto de interes a pagar. "))
-    }
+  } else if (metodoDePago === 2){
+    var cantidadDeCuotas = parseInt(prompt(" Indiquenos la cantidad de cuotas con las que desea abonar, ( siendo valores validos 3, 6, 9 y 12 ) para confirmarle el monto de interes a pagar. "))
+  }
 
 
 // cantidad de cuotas a pagar
 
 switch (cantidadDeCuotas) {
   case 3:
-    alert("El porcentaje de intereses a pagar es 0%")
-    var interesAPagar =  '0';
-    console.log("Cliente cancela en 3 cuotas sin interes")
-    break
+  alert("El porcentaje de intereses a pagar es 0%")
+  var interesAPagar =  '0';
+  console.log("Cliente cancela en 3 cuotas sin interes")
+  break
 
-    case 6:
-    alert("El porcentaje de intereses a pagar es 15%")
-    var interesAPagar =  '15';
-    console.log("Cliente cancela en 6 cuotas con un recargo del 15%")
-    break
+  case 6:
+  alert("El porcentaje de intereses a pagar es 15%")
+  var interesAPagar =  '15';
+  console.log("Cliente cancela en 6 cuotas con un recargo del 15%")
+  break
+  
+  case 9:
+  alert("El porcentaje de intereses a pagar es 30%")
+  var interesAPagar =  '30';
+  console.log("Cliente cancela en 9 cuotas con un recargo del 30%")
+  break
+
+  case 12:
+  alert("El porcentaje de intereses a pagar es 45%")
+  var interesAPagar =  '45';
+  console.log("Cliente cancela en 12 cuotas con un recargo del 45%")
+  break
+
+  default:
+    alert("Cantidad de cuotas seleccionas NO PERMITIDAS")
+    console.log("Cliente selecciono un numero no permitido de cuotas.")
     
-    case 9:
-    alert("El porcentaje de intereses a pagar es 30%")
-    var interesAPagar =  '30';
-    console.log("Cliente cancela en 9 cuotas con un recargo del 30%")
-    break
-
-    case 12:
-    alert("El porcentaje de intereses a pagar es 45%")
-    var interesAPagar =  '45';
-    console.log("Cliente cancela en 12 cuotas con un recargo del 45%")
-    break
-
-    default:
-      alert("Cantidad de cuotas seleccionas NO PERMITIDAS")
-      console.log("Cliente selecciono un numero no permitido de cuotas.")
-      
 }
 
 const cliente = new Cliente (
@@ -116,3 +116,46 @@ console.log( clientes.some((el) => el.intereses == '45'));
 }else{
   alert("ACCESO DENEGADO! Usted tiene que ser mayor de edad para poder continuar.")
 }
+
+//Implementacion de DOM
+const carrito = document.getElementsByClassName ("objetosCarrito")
+console.log(carrito)
+
+const sumarCarrito = document.getElementsByClassName ("sumarCarrito")
+console.log(sumarCarrito)
+
+let cantidadTelevisores60 = 0;
+let cantidadTelevisores40 = 0;
+let cantidadTelevisores32 = 0;
+
+let lista = []
+
+sumarCarrito[0].addEventListener ("click", respuestaClick1)
+  function respuestaClick1 () {
+    cantidadTelevisores60++;
+    carrito[0].innerText = cantidadTelevisores60;
+    lista[0] = (cantidadTelevisores60);
+    console.log(lista)
+    localStorage.setItem('carrito' , lista);
+  }
+
+sumarCarrito[1].addEventListener ("click", respuestaClick2)
+  function respuestaClick2 () {
+    cantidadTelevisores40++;
+    carrito[1].innerText = cantidadTelevisores40;
+    lista[1] = (cantidadTelevisores40);
+    console.log(lista)
+    localStorage.setItem('carrito' , lista);
+}
+
+sumarCarrito[2].addEventListener ("click", respuestaClick3)
+  function respuestaClick3 () {
+    cantidadTelevisores32++;
+    carrito[2].innerText = cantidadTelevisores32;
+    lista[2] = (cantidadTelevisores32);
+    console.log(lista)
+    localStorage.setItem('carrito' , lista);
+}
+
+let restoreCarrito = localStorage.getItem("carrito")
+console.log(restoreCarrito);
